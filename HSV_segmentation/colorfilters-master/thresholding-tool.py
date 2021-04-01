c8680c904775693dd16a1b6aa8c3afe04241f5b0
joshuacv <joshuacherian.v@gmail.com>
2021-04-01 19:38:44 +0200

    remove unused files

--- HSV_segmentation/colorfilters-master/thresholding-tool.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:345e712acfe841bf71adc907963db91558c205c4cf9ef1bfb4a7e985141b17ff
-size 2346
--- HSV_segmentation/colorfilters-master/visual_check_thresholds.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0c55397907a4a641d8ea1c585591dc4bfe510e17e9b7ad06f8277e15ee98f2af
-size 1493
